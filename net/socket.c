0ce33b78897d0df521e031eb40dbe668b0465b79
mananab11 <mananbaba11@gmail.com>
2021-02-17 19:13:38 -0500

    add comment in accept/connect

--- net/socket.c
+++ net/socket.c
@@ -1820,7 +1820,7 @@ SYSCALL_DEFINE4(accept4, int, fd, struct sockaddr __user *, upeer_sockaddr,
 
 SYSCALL_DEFINE3(accept, int, fd, struct sockaddr __user *, upeer_sockaddr,
 		int __user *, upeer_addrlen)
-{
+{	printk(KERN_INFO "SOCKET APPLICATION INVOKED accept sys CALL\n");
 	return __sys_accept4(fd, upeer_sockaddr, upeer_addrlen, 0);
 }
 
@@ -1878,7 +1878,7 @@ int __sys_connect(int fd, struct sockaddr __user *uservaddr, int addrlen)
 
 SYSCALL_DEFINE3(connect, int, fd, struct sockaddr __user *, uservaddr,
 		int, addrlen)
-{
+{	printk(KERN_INFO "SOCKET APPLICATION INVOKED connect() sys call\n");
 	return __sys_connect(fd, uservaddr, addrlen);
 }
 
